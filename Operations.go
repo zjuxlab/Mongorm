c222cd3d3cca72e5897cb729ec8ba238eb203c8c
cy <740310894@qq.com>
2022-07-13 15:54:06 +0800

    change return [ok bool] too [err error]

--- Operations.go
+++ Operations.go
@@ -1,156 +1,168 @@
 package Mongorm
 
 import (
+	"fmt"
+	"reflect"
+
 	. "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InsertOne[T any](val T, opts ...*options.InsertOneOptions) bool {
+type CollectionNotFoundError[T any] struct{}
+
+func (e CollectionNotFoundError[T]) Error() string {
+	var v T
+	typename := reflect.TypeOf(v).Name()
+
+	return fmt.Sprintf("[Mongorm] Cannot Find Collection for type (%s)", typename)
+}
+
+func InsertOne[T any](val T, opts ...*options.InsertOneOptions) error {
 	col := GetCollection[T]()
 	if col == nil {
-		return false
+		return CollectionNotFoundError[T]{}
 	}
 	_, err := col.InsertOne(ctx, val, opts...)
 	if err != nil {
 		logError[T]("Add", err)
 	}
-	return err == nil
+	return err
 }
 
-func FindOne[T any](filter D, opts ...*options.FindOneOptions) (val T, ok bool) {
+func FindOne[T any](filter D, opts ...*options.FindOneOptions) (val T, err error) {
 	col := GetCollection[T]()
 	if col == nil {
-		return val, false
+		return val, CollectionNotFoundError[T]{}
 	}
 	res := col.FindOne(ctx, filter, opts...)
 	if res.Err() != nil {
 		logError[T]("FindOne", res.Err())
-		return val, false
+		return val, res.Err()
 	}
 
 	if err := res.Decode(&val); err != nil {
 		logError[T]("FindOne", err)
-		return val, false
+		return val, err
 	}
 
-	return val, true
+	return val, nil
 }
 
-func FindMany[T any](filter D, opts ...*options.FindOptions) (vals []T, ok bool) {
+func FindMany[T any](filter D, opts ...*options.FindOptions) (vals []T, err error) {
 	col := GetCollection[T]()
 	if col == nil {
-		return nil, false
+		return nil, CollectionNotFoundError[T]{}
 	}
 	res, err := col.Find(ctx, filter, opts...)
 	if err != nil {
 		logError[T]("FindMany", err)
-		return nil, false
+		return nil, err
 	}
 
 	if err := res.All(ctx, &vals); err != nil {
 		logError[T]("FindMany", err)
-		return nil, false
+		return nil, err
 	}
 
-	return vals, true
+	return vals, nil
 }
 
-func Count[T any](filter D, opts ...*options.CountOptions) (cnt int64, ok bool) {
+func Count[T any](filter D, opts ...*options.CountOptions) (cnt int64, err error) {
 	col := GetCollection[T]()
 	if col == nil {
-		return -1, false
+		return -1, CollectionNotFoundError[T]{}
 	}
-	cnt, err := col.CountDocuments(ctx, filter, opts...)
+	cnt, err = col.CountDocuments(ctx, filter, opts...)
 	if err != nil {
 		logError[T]("Count", err)
-		return -1, false
+		return -1, err
 	}
-	return cnt, true
+	return cnt, nil
 }
 
-func DeleteOne[T any](filter D, opts ...*options.DeleteOptions) bool {
+func DeleteOne[T any](filter D, opts ...*options.DeleteOptions) error {
 	col := GetCollection[T]()
 	if col == nil {
-		return false
+		return CollectionNotFoundError[T]{}
 	}
 	_, err := col.DeleteOne(ctx, filter, opts...)
 	if err != nil {
 		logError[T]("DeleteOne", err)
-		return false
+		return err
 	}
-	return true
+	return nil
 }
 
-func DeleteMany[T any](filter D, opts ...*options.DeleteOptions) bool {
+func DeleteMany[T any](filter D, opts ...*options.DeleteOptions) error {
 	col := GetCollection[T]()
 	if col == nil {
-		return false
+		return CollectionNotFoundError[T]{}
 	}
 	_, err := col.DeleteMany(ctx, filter, opts...)
 	if err != nil {
 		logError[T]("DeleteMany", err)
-		return false
+		return err
 	}
-	return true
+	return nil
 }
 
-func ReplaceOne[T any](filter D, new_val T, opts ...*options.FindOneAndReplaceOptions) (old_val T, ok bool) {
+func ReplaceOne[T any](filter D, new_val T, opts ...*options.FindOneAndReplaceOptions) (old_val T, err error) {
 	col := GetCollection[T]()
 	if col == nil {
-		return old_val, false
+		return old_val, CollectionNotFoundError[T]{}
 	}
 	res := col.FindOneAndReplace(ctx, filter, new_val, opts...)
 	if res.Err() != nil {
 		logError[T]("ReplaceOne", res.Err())
-		return old_val, false
+		return old_val, res.Err()
 	}
 	if err := res.Decode(&old_val); err != nil {
 		logError[T]("ReplaceOne", err)
-		return old_val, false
+		return old_val, err
 	}
-	return old_val, true
+	return old_val, nil
 }
 
-func UpdateOne[T any](filter D, upd D, opts ...*options.UpdateOptions) bool {
+func UpdateOne[T any](filter D, upd D, opts ...*options.UpdateOptions) error {
 	col := GetCollection[T]()
 	if col == nil {
-		return false
+		return CollectionNotFoundError[T]{}
 	}
 	_, err := col.UpdateOne(ctx, filter, upd, opts...)
 	if err != nil {
 		logError[T]("UpdateOne", err)
-		return false
+		return err
 	}
-	return true
+	return nil
 }
 
-func UpdateMany[T any](filter D, upd D, opts ...*options.UpdateOptions) bool {
+func UpdateMany[T any](filter D, upd D, opts ...*options.UpdateOptions) error {
 	col := GetCollection[T]()
 	if col == nil {
-		return false
+		return CollectionNotFoundError[T]{}
 	}
 	_, err := col.UpdateMany(ctx, filter, upd, opts...)
 	if err != nil {
 		logError[T]("UpdateMany", err)
-		return false
+		return err
 	}
-	return true
+	return nil
 }
 
 // pipeline: bson.A or "Mongorm/PipelineBuilder".PB
-func Aggregate[T any, Ret any](pipeline A, opts ...*options.AggregateOptions) (ret []Ret, ok bool) {
+func Aggregate[T any, Ret any](pipeline A, opts ...*options.AggregateOptions) (ret []Ret, err error) {
 	col := GetCollection[T]()
 	if col == nil {
-		return nil, false
+		return nil, CollectionNotFoundError[T]{}
 	}
 	res, err := col.Aggregate(ctx, pipeline, opts...)
 	if err != nil {
 		logError[T]("Aggregate", err)
-		return nil, false
+		return nil, err
 	}
 	if err := res.All(ctx, &ret); err != nil {
 		logError[T]("Aggregate", err)
-		return nil, false
+		return nil, err
 	}
-	return ret, true
+	return ret, nil
 }
